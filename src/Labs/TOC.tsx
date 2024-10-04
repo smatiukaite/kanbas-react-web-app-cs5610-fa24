ca33e2400fd21120ac9d69bdb06dda2bde970a92
smatiukaite <matiukaite.s@northeastern.edu>
2024-10-03 23:06:29 -0400

    Add github repo to TOC

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -35,7 +35,7 @@ export default function TOC() {
                 </a>
             </li>
             <li className="nav-item">
-                <a id="wd-k" href="https://github.com/smatiukaite" className="nav-link">
+                <a id="wd-github" href="https://github.com/smatiukaite" className="nav-link">
                     My GitHub
                 </a>
             </li>
