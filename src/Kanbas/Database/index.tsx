c6e1fa82c9e73ed7e9474ed9885518d9d9cf23f9
smatiukaite <matiukaite.s@northeastern.edu>
2024-10-14 19:54:27 -0400

    Commit 3.9

--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,3 +1,7 @@
 import courses from "./courses.json";
 import modules from "../Courses/Modules/modules.json";
-export { courses, modules };
+import assignments from "../Courses/Assignments/assignments.json";
+import users from "./users.json";
+import enrollments from "./enrollments.json";
+
+export { courses, modules, assignments, users, enrollments };
