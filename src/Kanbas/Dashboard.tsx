f1421a8b588d17d444e03e519ade2ecd8a6bb4f3
smatiukaite <matiukaite.s@northeastern.edu>
2024-10-30 11:53:40 -0400

    Commit 3.1.1

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,11 +1,42 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as db from "./Database";
 
 export default function Dashboard() {
-    const courses = db.courses;
+    const [courses, setCourses] = useState<any[]>(db.courses);
+    const [course, setCourse] = useState<any>({
+        _id: "0", 
+        name: "New Course", 
+        number: "New Number",
+        startDate: "2023-09-10", 
+        endDate: "2023-12-15",
+        picture: "react1.jfif", 
+        description: "New Description"
+    });
+    const addNewCourse = () => {
+        const newCourse = {
+            ...course,
+            _id: new Date().getTime().toString()
+        };
+        setCourses([...courses, { ...course, ...newCourse }]);
+    };
+
     return (
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+
+            {/* NEW COURSE BUTTON */}
+            <h5>New Course
+                <button className="btn btn-primary float-end"
+                    id="wd-add-new-course-click"
+                    onClick={addNewCourse} > Add </button>
+            </h5><hr /><br />
+            {/* ADD NEW COURSE AND NEW DESCRIPTION FOR A COURSE */}
+            <input defaultValue={course.name} className="form-control mb-2"
+                onChange={(e) => setCourse({ ...course, name: e.target.value })} />
+            <textarea defaultValue={course.description} className="form-control"
+                onChange={(e) => setCourse({ ...course, description: e.target.value })} /><br />
+
             <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div id="wd-dashboard-courses" className="row">
                 <div className="row row-cols-1 row-cols-md-5 g-4">
