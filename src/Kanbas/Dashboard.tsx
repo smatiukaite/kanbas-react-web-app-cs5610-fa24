00e8370708239e74583172b3e4133a78b0f2d86c
smatiukaite <matiukaite.s@northeastern.edu>
2024-11-05 21:12:44 -0500

    Commit changes3

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 // import * as db from "./Database";
 import { useState, useEffect } from "react";
 
