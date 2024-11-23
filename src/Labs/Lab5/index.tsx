1688067365e6724a712d8dfda7fa02c7fa7fe597
smatiukaite <matiukaite.s@northeastern.edu>
2024-11-23 01:23:08 -0500

    Commit until 3.4 except the do yourself part

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,4 +1,8 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithObjects from "./WorkingWithObjects";
+
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
     return (
@@ -10,6 +14,9 @@ export default function Lab5() {
                 </a>
             </div><hr />
             <EnvironmentVariables />
+            <PathParameters />
+            <QueryParameters />
+            <WorkingWithObjects />
         </div>
     );
 }
