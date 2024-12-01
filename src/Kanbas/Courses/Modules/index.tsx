40dd378d36e08d83736eec69ddabeac03569e21e
smatiukaite <matiukaite.s@northeastern.edu>
2024-12-01 15:45:34 -0500

    Commit everything up to 4.6

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,17 +1,44 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+import { setModules, addModule, editModule, updateModule, deleteModule } from "./reducer";
 import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import * as coursesClient from "../client";
+import * as modulesClient from "./client";
 
 export default function Modules() {
   const { cid } = useParams<{ cid: string }>();
   const [moduleName, setModuleName] = useState("");
   const modules = useSelector((state: any) => state.modulesReducer.modules);
   const dispatch = useDispatch();
+
+  const saveModule = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
+
+  const removeModule = async (moduleId: string) => {
+    await modulesClient.deleteModule(moduleId);
+    dispatch(deleteModule(moduleId));
+  };
+
+  const createModuleForCourse = async () => {
+    if (!cid) return;
+    const newModule = { name: moduleName, course: cid };
+    const module = await coursesClient.createModuleForCourse(cid, newModule);
+    dispatch(addModule(module));
+  };
+
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const userRole = currentUser?.role;
+  const fetchModules = async () => {
+    const modules = await coursesClient.findModulesForCourse(cid as string);
+    dispatch(setModules(modules));
+  };
+  useEffect(() => {
+    fetchModules();
+  }, []);
 
   if (userRole === "FACULTY") {
     return (
@@ -19,16 +46,16 @@ export default function Modules() {
         <ModulesControls
           moduleName={moduleName}
           setModuleName={setModuleName}
-          addModule={() => {
-            dispatch(addModule({ name: moduleName, course: cid }));
-            setModuleName("");
-          }}
+          addModule={createModuleForCourse}
+        // addModule={() => {
+        //   dispatch(addModule({ name: moduleName, course: cid }));
+        //   setModuleName("");
+        // }}
         />
 
         {/* Home module list */}
         <ul id="wd-modules" className="list-group rounded-0 p-0 mb-3 fs-5 m-3 border-gray">
           {modules
-            .filter((module: any) => module.course === cid)
             .map((module: any) => (
               <li key={module._id} className="list-group-item">
 
@@ -41,7 +68,8 @@ export default function Modules() {
                     }
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        dispatch(updateModule({ ...module, editing: false }));
+                        // dispatch(updateModule({ ...module, editing: false }));
+                        saveModule({ ...module, editing: false });
                       }
                     }}
                     defaultValue={module.name}
@@ -50,7 +78,7 @@ export default function Modules() {
 
                 <ModuleControlButtons
                   moduleId={module._id}
-                  deleteModule={() => dispatch(deleteModule(module._id))}
+                  deleteModule={(moduleId) => removeModule(moduleId)}
                   editModule={() => dispatch(editModule(module._id))}
                 />
               </li>
@@ -67,16 +95,16 @@ export default function Modules() {
         <ModulesControls
           moduleName={moduleName}
           setModuleName={setModuleName}
-          addModule={() => {
-            dispatch(addModule({ name: moduleName, course: cid }));
-            setModuleName("");
-          }}
+          addModule={createModuleForCourse}
+        // addModule={() => {
+        //   dispatch(addModule({ name: moduleName, course: cid }));
+        //   setModuleName("");
+        // }}
         />
 
         {/* Home module list */}
         <ul id="wd-modules" className="list-group rounded-0 p-0 mb-3 fs-5 m-3 border-gray">
           {modules
-            .filter((module: any) => module.course === cid)
             .map((module: any) => (
               <li key={module._id} className="list-group-item">
 
@@ -89,7 +117,8 @@ export default function Modules() {
                     }
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        dispatch(updateModule({ ...module, editing: false }));
+                        // dispatch(updateModule({ ...module, editing: false }));
+                        saveModule({ ...module, editing: false });
                       }
                     }}
                     defaultValue={module.name}
